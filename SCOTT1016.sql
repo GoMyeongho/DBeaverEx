fae0492a04168880c7bed5882c7caba40d5c1de5
GoMyeongho <sdx02013@ajou.ac.kr>
2024-10-16 12:42:26 +0900

    2024/10/16  12:42

--- SCOTT1016.sql
+++ SCOTT1016.sql
@@ -125,44 +125,157 @@ SELECT '['||TRIM('  _ORACLE_  ') || ']' AS TRIM,
 	'['||RTRIM('  _ORACLE_  ') || ']' AS RTRIM
 FROM dual;
 
+-- 날짜 데이터를 다루는 함수
+-- 날짜 데이터 + 숫자 : 가능, 날짜에서 숫자만큼의 이 후 날짜
+-- 날짜 데이터 - 숫자 : 가능, 날짜에서 숫자만큼의 이 전 날짜
+-- 날짜 데이터 - 날짜 데이터 : 가능, 두 날짜간의 일수 차이
+-- 날짜 데이터 + 날짜 데이터 : 연산 불가
+-- SYSDATE : 운영체제로 부터 시간을 가져오는 함수.
+SELECT SYSDATE FROM dual;	-- 오라클 함수라 오류로 뜸
 
 
+SELECT SYSDATE AS "현재 시간", 
+	SYSDATE - 1 AS "어제",
+	SYSDATE + 1 AS "내일"
+FROM dual;
+
+-- 몇 개월 이후 날짜를 구하는 ADD_MONTHS 함수 : 특정 날짜에 지정한 개월 수 이후 날짜 데어터를 반환
+
+SELECT SYSDATE,
+	ADD_MONTHS(SYSDATE, 3) AS "3개월 이후 날짜데이터"	-- 현재 시간으로부터 3개월 이후 날짜 데이터 반환  
+	FROM dual;
+
+-- 입사 10주년이 되는 사원들의 데이터 출력하기 (입사일로 부터 10년이 경과한 날짜 데어터 반환)
+SELECT empno, ename, hiredate AS "입사일", ADD_MONTHS(hiredate, 10 * 12) AS "10주년"
+FROM emp;
+
+-- 두 날짜간의 개월수 차이를 구하는 MONTHS_BETWEEN 함수 : 
+SELECT empno, ename, hiredate, sysdate,
+	MONTHS_BETWEEN(hiredate, sysdate) AS "-재직 기간",
+	MONTHS_BETWEEN(sysdate, hiredate) AS "재직 기간",
+	TRUNC(MONTHS_BETWEEN(sysdate, hiredate)) AS "재직 기간 2" 
+	FROM emp;
+
+-- 돌아오는 요일(NEXT_DAY), 달의 마지막 날짜(LAST_DAY)
+SELECT SYSDATE,
+	NEXT_DAY(SYSDATE, '월요일'),
+	LAST_DAY(SYSDATE)
+	FROM dual;
+
+SELECT LAST_DAY('24/8/28') FROM dual;
+
+-- 날짜 정보 추출 함수 : EXTRACT()
+SELECT EXTRACT (YEAR FROM DATE '2024-10-16')
+	FROM dual;
+
+SELECT * FROM EMP
+	WHERE EXTRACT (MONTH FROM HIREDATE) = 12;
 
 
+-- 자료형을 변환하는 형 변환 함수
+SELECT empno, ename, empno + '500', '23445' + '123'	-- 오라클의 기본 형변환 변경가능 숫자로 변환, 'abcd'는 오류생성
+FROM EMP 
+WHERE ename = 'FORD';
 
 
+-- 날짜, 숫자를 문자로 변환하는 TO_CHAR 함수 : 자바의 SimpleDateFormat
+SELECT SYSDATE AS "기본 시간 형태", TO_CHAR(SYSDATE, 'YYYY/MM/DD HH24:MI:SS') AS "현재날짜"
+FROM dual;
 
 
+SELECT SYSDATE,
+    TO_CHAR(SYSDATE, 'CC') AS 세기,
+    TO_CHAR(SYSDATE, 'YY') AS 연도,
+    TO_CHAR(SYSDATE, 'YYYY/MM/DD PM HH:MI:SS ') AS "년/월/일 시:분:초",
+    TO_CHAR(SYSDATE, 'Q') AS 쿼터,
+    TO_CHAR(SYSDATE, 'DD') AS 일,
+    TO_CHAR(SYSDATE, 'DDD') AS 경과일,
+    TO_CHAR(SYSDATE, 'HH') AS "12시간제",
+    TO_CHAR(SYSDATE, 'HH12') AS "12시간제",
+    TO_CHAR(SYSDATE, 'HH24') AS "24시간제",
+    TO_CHAR(SYSDATE, 'W') AS 몇주차
+FROM DUAL;
+
+SELECT SYSDATE,
+     TO_CHAR(SYSDATE, 'MM') AS MM,
+     TO_CHAR(SYSDATE, 'MON', 'NLS_DATE_LANGUAGE = KOREAN' ) AS MON_KOR,
+     TO_CHAR(SYSDATE, 'MON', 'NLS_DATE_LANGUAGE = JAPANESE') AS MON_JPN,
+     TO_CHAR(SYSDATE, 'MON', 'NLS_DATE_LANGUAGE = ENGLISH' ) AS MON_ENG,
+     TO_CHAR(SYSDATE, 'MONTH', 'NLS_DATE_LANGUAGE = KOREAN' ) AS MONTH_KOR,
+     TO_CHAR(SYSDATE, 'MONTH', 'NLS_DATE_LANGUAGE = JAPANESE') AS MONTH_JPN,
+     TO_CHAR(SYSDATE, 'MONTH', 'NLS_DATE_LANGUAGE = ENGLISH' ) AS MONTH_ENG
+FROM DUAL;
+
+-- 숫자 데이터 형식을 지정하여 출력하기
+SELECT sal,
+	TO_CHAR(sal, '$999,999') AS sal_$,		-- 9는 숫자의 한 자리를 의미하고 빈자리를 채우지 않음
+	TO_CHAR(sal, 'L999,999') AS sal_L,		-- 지역화폐 단위로 출력
+	TO_CHAR(sal, '999,999.00') AS sal_1,	-- 0은 빈자리를 0으로 채움
+	TO_CHAR(sal, '000,999,999.00') AS sal_2
+	FROM emp;
 
 
+-- TO_NUMBER : 숫자 타입의 문자열을 숫자 데이터로 변환해주는 함수
+SELECT 1300 - '1500', TO_NUMBER('1300') + '1500'
+FROM dual; -- 자동으로 변환되어서 사용할 필요 없음
+
+-- TO_DATE : 문자열로 명시된 날짜로 변환하는 함수
+SELECT TO_DATE('24/10/24') AS "날짜타입1",
+	TO_DATE('20240714','YYYY/MM/DD') AS "날짜타입2"
+FROM dual
+
+-- 1981년 7월 1일 이후 입사한 사원 정보 출력하기
+SELECT * FROM EMP
+	WHERE HIREDATE > '19810701'; -- TO_DATE를 안써도 자동으로 해줌
+	-- 원칙적으론 : HIREDATE > TO_DATE('19810701', 'YYYY/MM/DD')
+
+-- NULL 처리 함수 : 특정 열의 행에 데이터가 없는 경우 데이터값이 NULL이 됨 (NULL 값이 없음)
+-- NULL : 값이 할당되지 않았기 때문에 공백이나 0과는 다른 의미, 연산(계산, 비교 등등) 불가
+-- NVL(검사할 데이터 또는 열, 앞의 데이터가 NULL인 경우 대체할 값)
+
+SELECT empno, ename, sal, comm, sal * 12 + comm,
+	NVL(comm, 0),
+	sal * 12 + NVL(comm,0)
+	FROM emp;
+
+-- NVL2(검사할 데이터, 데이터가 NULL 이 아닐때 반환 되는 값, 데이터가 NULL 일 때 반환되는 값) 
+SELECT empno, ename, comm,
+	NVL2(comm,'O','X'),
+	NVL2(comm, sal * 12 + comm, sal * 12) AS "연봉"
+	FROM emp;
 
 
+-- NULLIF : 두 값을 비겨하여 동일하면 NULL, 동일하지 않으면 첫번째 값을 반환
+SELECT NULLIF (10, 10), NULLIF ('A', 'B')
+FROM dual;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-- DECODE : 주어진 데이터 값이 조건 값과 일치하는 값을 출력하고 일치하는 값이 없으면 기본값을 출력합니다
+SELECT empno, ename, job, sal,
+	DECODE(job,
+	'MANAGER', sal * 1.1,
+	'SALESMAN', sal *1.05,
+	'ANALYST', sal,
+	sal * 1.03) AS "연봉인상"
+FROM emp;
 	
-	
-	
-	
-	
+-- CASE : SQL의 표준 함수, 일반적으로 SELECT절에서 많이 사용됨
+SELECT	empno, ename, job, sal,
+		CASE job
+			WHEN 'MANAGER' THEN sal * 1.1
+			WHEN 'SALESMAN' THEN sal * 1.05
+			WHEN 'ANALYST' THEN sal
+			ELSE sal * 1.03
+		END AS "연봉인상"
+	FROM emp;
+
+
+-- 열 값에 따라서 출력이 달라지는 CASE 문 : 기준 데이터를 지정하지 않고 사용하는 방법
+SELECT empno, ename, comm,
+		CASE 
+			WHEN comm IS NULL THEN '해당 사항 없음'
+			WHEN comm = 0 THEN '수당 없음'
+			WHEN comm > 0 THEN '수당 : '|| comm
+		END AS "수당 정보"
+	FROM emp;
 	
 	
